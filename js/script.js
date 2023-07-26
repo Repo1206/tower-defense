9ec9818978e032da5313f762738efd247024c345
Repo1206 <pestes1206@gmail.com>
2023-07-26 13:00:11 +0700

    new file:   img/map.png 	modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -6,3 +6,8 @@ canvas.height = 768
 
 c.fillStyle = 'white'
 c.fillRect(0, 0, canvas.width, canvas.height)
+const image = new Image()
+image.onload = () => {
+    c.drawImage(image, 0, 0)
+}
+image.src = "img/map.png"
